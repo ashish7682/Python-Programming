e72736e5f4879b2cc95cd62faab2a4e36ac76152
Ashish Kumar Sahoo <ashishkumarsahoo2023@gmail.com>
2024-03-16 22:46:37 +0530

    intial commit

---
+++ Input_Output/Output.py
@@ -0,0 +1,27 @@
+#---------Type-1----------#
+
+print("Hello")
+
+#---------Type-2----------#
+
+print("Ankit","Ashish","Abhisekh")
+
+#---------Type-3----------#
+
+print("Ankit","Ashish","Abhisekh",sep='-')
+
+print("Ankit","Ashish","Abhisekh",sep='_')
+
+print("Ankit","Ashish","Abhisekh",sep=',')
+
+print("Ankit","Ashish","Abhisekh",sep='*')
+
+#---------Type-4----------#
+
+print("Hello",end=" ")
+print("World")
+
+#---------Type-5----------#
+
+print("Hello",end="\n\n")
+print("World")
---
+++ Input_Output/input.py
@@ -0,0 +1,25 @@
+#-------Type-1------------#
+
+var=input("Enter Data: ")
+print(var)
+print(type(var)) #----By default input type is string datatype in python---#
+
+#-------Type-2-----------#
+     #......Type casting in python...........#
+
+var=int(input("Enter Data: "))
+print(var)
+print(type(var))
+
+#-------Type-3-----------#
+     #......Type casting in python...........#
+
+var=float(input("Enter Data: "))
+print(var)
+print(type(var))
+
+#--------Type-4----------#
+     #...........Eval function in python......#
+var=eval(input("Enter Data: "))
+print(var)
+print(type(var))
