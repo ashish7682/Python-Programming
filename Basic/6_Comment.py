e17bc01f606504c39848f66cf7099a52298eacce
Ashish Kumar Sahoo <ashishkumarsahoo2023@gmail.com>
2024-03-19 23:20:18 +0530

    intial commit

---
+++ Basic/6_Comment.py
@@ -0,0 +1,8 @@
+#  We are going to learn python comment----------->This is single line comment
+
+""""
+   Andrew R. Jassy (born January 13, 1968) is an American business executive who is the president and
+   chief executive officer (CEO) of Amazon. Before being appointed by Jeff Bezos and the Amazon board
+   during the fourth quarter of 2020, Jassy was the SVP and later CEO of Amazon Web Services from 2003 to 2021."""
+
+#---------->This is multi line comment
---
+++ Basic/7_Keyword.py
@@ -0,0 +1,9 @@
+#----------Write  a program to print all the keyword list----------#
+import keyword
+
+# Get all the keywords
+keywords=keyword.kwlist
+
+#Print all the Keywords
+for kw in keywords:
+    print(kw)
