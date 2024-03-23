b65eac3f633f28117cc65d225c89a1889909b64e
Ashish Kumar Sahoo <ashishkumarsahoo2023@gmail.com>
2024-03-23 23:49:14 +0530

    intial commit

--- Datatype/tuple.py
+++ Datatype/tuple.py
@@ -1,3 +1,31 @@
-var=()
-print(type(var))
+# Creating a tuple
+my_tuple = (1, 2, 3, 'a', 'b', 'c')
 
+# Accessing elements of a tuple
+print("First element:", my_tuple[0])
+print("Second element:", my_tuple[1])
+print("Last element:", my_tuple[-1])
+
+# Slicing a tuple
+print("Slice from index 2 to 4:", my_tuple[2:5])
+
+# Tuple unpacking
+x, y, z, *rest = my_tuple
+print("Unpacked values:")
+print("x:", x)
+print("y:", y)
+print("z:", z)
+print("Rest:", rest)
+
+# Immutable nature of tuples - you can't modify them
+# This would cause an error:
+# my_tuple[0] = 5
+
+# Length of a tuple
+print("Length of the tuple:", len(my_tuple))
+
+# Count occurrences of an element in a tuple
+print("Number of occurrences of 'a':", my_tuple.count('a'))
+
+# Finding the index of an element in a tuple
+print("Index of 'b':", my_tuple.index('b'))
